0664f5bbb9df38135f68c4123281ad83495b6bc2
Kacper Staszek <kacperstaszek1995@gmail.com>
2019-02-21 22:10:15 +0100

    Implemented Franc != Dollar

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -4,10 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
-class DollarTest {
+public class MoneyTest {
     @Test
-    void testMultiplication(){
+   void testFrancMultiplication(){
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10),five.times(2));
+        assertEquals(new Franc(15),five.times(3));
+    }
+    @Test
+    void testDollarMultiplication(){
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10),five.times(2));
         assertEquals(new Dollar(15),five.times(3));
@@ -16,6 +21,9 @@ class DollarTest {
     void testEquality(){
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
+        assertFalse(new Franc(5).equals(new Dollar(5)));
     }
 
 }

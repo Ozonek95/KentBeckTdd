e1833c4fef9aa5debd1cced6783d9d7546b0328d
Kacper Staszek <kacperstaszek1995@gmail.com>
2019-02-21 21:45:09 +0100

    next refactor

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,15 +1,21 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-public class DollarTest {
+class DollarTest {
     @Test
-    public void testMultiplication(){
+    void testMultiplication(){
         Dollar five = new Dollar(5);
-        Dollar prouct = five.times(2);
-        assertEquals(10,prouct.amount);
-        prouct=five.times(3);
-        assertEquals(15,prouct.amount);
+        assertEquals(new Dollar(10),five.times(2));
+        assertEquals(new Dollar(15),five.times(3));
     }
+    @Test
+    void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
 }
